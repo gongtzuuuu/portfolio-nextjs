d761300347fd7984dee5450b701a64213db8ada4
lizzy <lizzy@jgallop.com>
2024-06-08 13:12:51 +0800

    feat. Add contents in Home page.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+import { useTranslations, useLocale } from 'next-intl';
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
   const t = useTranslations('HomePage');
+  const activeLocale = useLocale();
   return (
-    <div id="home-page" className="container flex flex-col items-center">
-      <h1 className="text-4xl font-bold">{t('title')}</h1>
-      <p>{t('description')}</p>
+    <div
+      id="home-page"
+      className="container flex flex-col space-y-6 items-center"
+    >
+      <h1 className="text-3xl md:text-4xl font-bold text-center">
+        Hello, I am Tzu-Yun Liang
+      </h1>
+      <p className="text-center">
+        I am a software engineer & product designer
+        <br />
+        who loves to deliver high-quality digital solutions.
+      </p>
+      <div className="flex space-x-6">
+        <Link href={`${activeLocale}/work`}>see my project</Link>
+        <Link href={`${activeLocale}/about`}>more about me</Link>
+      </div>
     </div>
   );
 };
