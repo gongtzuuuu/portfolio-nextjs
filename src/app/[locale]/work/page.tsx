d7a0427f8919cb31f20602b24513285c47bfbf5f
lizzy <lizzy@jgallop.com>
2024-06-15 09:33:55 +0800

    feat. Modify the layout of work page.

--- src/app/[locale]/work/page.tsx
+++ src/app/[locale]/work/page.tsx
@@ -9,7 +9,7 @@ const WorkPage: React.FC<WorkPageProps> = ({}) => {
   const t = useTranslations('WorkPage');
   return (
     <div className="h-full flex items-end">
-      <div className="h-[200px] w-[50%] hidden md:flex p-4">
+      <div className="h-[200px] w-[50%] hidden md:flex px-4 pt-4">
         <Image
           src={PlaceHolderImg}
           alt="Menu Image"
@@ -18,25 +18,39 @@ const WorkPage: React.FC<WorkPageProps> = ({}) => {
           height={500}
         />
       </div>
-      <div className="w-full md:w-[50%] flex flex-col space-y-4 px-0 md:p-4">
-        <h3 className="text-4xl font-bold">work</h3>
+      {/** WORK EXPERIENCE LIST */}
+      <div className="w-full md:w-[50%] max-h-96 overflow-y-scroll no-scrollbar flex flex-col space-y-4 px-0 md:p-4">
+        <div className="space-y-2">
+          <h3 className="text-4xl font-bold">work</h3>
+        </div>
         <hr />
-        <h3 className="text-3xl font-bold">
-          <span className="text-base font-extralight mr-2">01</span>meloverse
-        </h3>
+        <div className="space-y-2">
+          <h3 className="text-3xl font-bold">meloverse</h3>
+          <p className="text-xs font-extralight mr-2">
+            June 2023 / Personal Project
+          </p>
+        </div>
         <hr />
-        <h3 className="text-3xl font-bold">
-          <span className="text-base font-extralight mr-2">02</span>GluttonGlobe
-        </h3>
+        <div className="space-y-2">
+          <h3 className="text-3xl font-bold">GluttonGlobe</h3>
+          <p className="text-xs font-extralight mr-2">
+            June 2023 / Collaboration Project
+          </p>
+        </div>
         <hr />
-        <h3 className="text-3xl font-bold">
-          <span className="text-base font-extralight mr-2">03</span>FitQuest
-        </h3>
+        <div className="space-y-2">
+          <h3 className="text-3xl font-bold">FitQuest</h3>
+          <p className="text-xs font-extralight mr-2">
+            June 2023 / Collaboration Project
+          </p>
+        </div>
         <hr />
-        <h3 className="text-3xl font-bold">
-          <span className="text-base font-extralight mr-2">04</span>Office
-          Simulator
-        </h3>
+        <div className="space-y-2">
+          <h3 className="text-3xl font-bold">Office Simulator</h3>
+          <p className="text-xs font-extralight mr-2">
+            June 2023 / Personal Project
+          </p>
+        </div>
       </div>
     </div>
   );
