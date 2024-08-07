f655c4091b15de865cabfb0747678744661e729c
lizzy <lizzy@jgallop.com>
2024-08-07 16:26:10 +0800

    feat. Add Button to NotFoundPage.

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import '@/styles/notfound.css';
 
 const NotFound = () => {
   const t = useTranslations('NotFoundPage');
-  const pageTitle = t('title');
-  const pageDescription = t('description');
   return (
-    <div className="flex flex-col items-center space-y-2">
-      <h3 className="text-4xl font-bold">{pageTitle}</h3>
-      <p>{pageDescription}</p>
+    <div className="flex flex-col items-center">
+      <h3 className="text-4xl font-bold mb-2">{t('title')}</h3>
+      <p className="text-center mb-5">{t('description')}</p>
+      <Link className="glow-button" href="/">
+        {t('link')}
+      </Link>
     </div>
   );
 };
