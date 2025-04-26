2d86762607b34bee01d3bfa5ca40b13958d6a694
Lizzy Liang <liangtzuyun@gmail.com>
2025-04-26 11:11:28 +0800

    refactor: works

--- src/lib/url-utils.ts
+++
@@ -1,7 +0,0 @@
-export const workLabelToSegment = (workLabel: string) => {
-  return workLabel.toLowerCase().replace(/\s/g, '-');
-};
-
-export const segmentToWorkLabel = (segment: string) => {
-  return segment.replace(/-/g, ' ');
-};
